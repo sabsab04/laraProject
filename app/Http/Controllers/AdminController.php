26bf829d419b5068499ef0a72db7e8946e81dbb5
MoniqueLorenza <moniquelorensa@icloud.com>
2026-06-11 23:38:44 +0200

    Fix: aggiunto use OrganizerRequest in AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use App\Models\OrganizerRequest;
 
 class AdminController extends Controller
 {
