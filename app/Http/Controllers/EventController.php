d693387e8979cde054479e8042a0ed064640d197
MoniqueLorenza <moniquelorensa@icloud.com>
2026-06-11 19:04:21 +0200

    ricerca eventi, barra rimossa dal layout

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,46 +8,30 @@ use Illuminate\Http\Request;
 class EventController extends Controller
 {
     // Mostra tutti gli eventi nella pagina principale
-    public function index(Request $request)
-    {
-        $query = Event::query();
+   public function index(Request $request)
+{
+    $query = Event::query();
 
-        if ($request->filled('ricerca')) {
-            $termine = $request->ricerca;
+    if ($request->filled('ricerca')) {
+        $termine = $request->ricerca;
 
-            // Passiamo $termine dentro la closure con 'use'
-            $query->where(function($q) use ($termine) {
-                
-                if (str_contains($termine, '*')) {
-                    // C'è l'asterisco: ricerca nella descrizione
-                    $termini = explode(' ', $termine);
-
-                    // Passiamo $termini dentro questa seconda closure
-                    $q->where(function($subQuery) use ($termini) {
-                        foreach ($termini as $index => $t) {
-                            $wildcardTerm = str_replace('*', '%', $t);
-                            
-                            if ($index === 0) {
-                                $subQuery->where('descrizione', 'LIKE', $wildcardTerm);
-                            } else {
-                                $subQuery->orWhere('descrizione', 'LIKE', $wildcardTerm);
-                            }
-                        }
-                    });
-                } else {
-                    // Niente asterisco: ricerca esatta sulla città
-                    // $termine qui è già disponibile perché lo abbiamo passato nel primo 'use'
-                    $q->where('citta', $termine);
-                }
-            });
+        if (Auth::check()) {
+            // Utente registrato: cerca per città o descrizione con *
+            if (str_contains($termine, '*')) {
+                $wildcardTerm = str_replace('*', '%', $termine);
+                $query->where('descrizione', 'LIKE', $wildcardTerm);
+            } else {
+                $query->where('citta', $termine);
+            }
+        } else {
+            // Utente non registrato: solo città esatta
+            $query->where('citta', $termine);
         }
-
-        $eventi = $query->paginate(2);
-        
-        // Passiamo i risultati alla vista
-        return view('eventi', compact('eventi'));
     }
 
+    $eventi = $query->paginate(5);
+    return view('eventi', compact('eventi'));
+}
     // Mostra il dettaglio di un singolo evento in base al suo ID
    public function show($id)
 {
